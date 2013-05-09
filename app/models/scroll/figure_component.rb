fd4f32b34cbbe12c2268e24fd7dbdc6e7b768458
Robin Clart <robin@clart.be>
2013-05-09 10:33:33 +0700

    Nasty fix for paperclip option

--- app/models/scroll/figure_component.rb
+++ app/models/scroll/figure_component.rb
@@ -4,20 +4,9 @@ module Scroll
 
     ATTACHMENT_STYLES = { large: "1880x1880>", thumb: "100x100>" }
 
-    if Rails.env.development?
-      has_attached_file :image, styles: ATTACHMENT_STYLES
-    else
-      has_attached_file :image,
-        styles: ATTACHMENT_STYLES,
-        storage: :fog,
-        fog_credentials: {
-          provider: "AWS",
-          aws_access_key_id: ENV["S3_KEY"],
-          aws_secret_access_key: ENV["S3_SECRET"],
-          region: ENV["S3_REGION"]
-        },
-        fog_directory: ENV["S3_BUCKET"]
-    end
+    has_attached_file :image, PAPERCLIP_OPTIONS.merge({
+      styles: ATTACHMENT_STYLES
+    })
 
     def presentable?
       image.present?
